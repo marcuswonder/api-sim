8263dcce555c2e0f4132ce7fe6c2fb44f8f1f889
marcuswonder <marcus.bruce.smith@gmail.com>
2025-10-03 16:44:48 +0100

    Added handling for params as well as body

--- controllers/bob/get-conversation.js
+++ controllers/bob/get-conversation.js
@@ -1,9 +1,93 @@
 function getConversation(req, res) {
+    console.log("req.params in getConversation", req.params);
     console.log("req.body in getConversation", req.body);
-    res.json({ message: `getConversation req.body: ${JSON.stringify(req.body)}` });
+    res.json({ message: `getConversation req.params: ${req.params} || req.body: ${JSON.stringify(req.body)}` });
 
 }
 
 export { getConversation };
 
-const conversation1 = {}
+const conversations = [
+    {
+        "data": [
+            {
+            "conversation_id": "2010000000000000001",
+            "author_id": "300100001",
+            "created_at": "2025-10-03T18:00:00.000Z",
+            "id": "2010000000000000001",
+            "text": "Name one thing that will 💯 happen in the Premier League game between Bournemouth and Fulham tonight. #BOUFUL"
+            },
+            {
+            "conversation_id": "2010000000000000001",
+            "in_reply_to_user_id": "300100001",
+            "author_id": "300100002",
+            "created_at": "2025-10-03T18:01:00.000Z",
+            "id": "2010000000000000002",
+            "referenced_tweets": [{ "type": "replied_to", "id": "2010000000000000001" }],
+            "text": "@Fred Over three goals",
+            "entities": { "mentions": [{ "start": 0, "end": 5, "username": "Fred", "id": "300100001" }] }
+            },
+            {
+            "conversation_id": "2010000000000000001",
+            "in_reply_to_user_id": "300100001",
+            "author_id": "300100003",
+            "created_at": "2025-10-03T18:02:00.000Z",
+            "id": "2010000000000000003",
+            "referenced_tweets": [{ "type": "replied_to", "id": "2010000000000000001" }],
+            "text": "@Fred Draw game",
+            "entities": { "mentions": [{ "start": 0, "end": 5, "username": "Fred", "id": "300100001" }] }
+            },
+            {
+            "conversation_id": "2010000000000000001",
+            "in_reply_to_user_id": "300100001",
+            "author_id": "300100004",
+            "created_at": "2025-10-03T18:03:00.000Z",
+            "id": "2010000000000000004",
+            "referenced_tweets": [{ "type": "replied_to", "id": "2010000000000000001" }],
+            "text": "@Fred One team will score",
+            "entities": { "mentions": [{ "start": 0, "end": 5, "username": "Fred", "id": "300100001" }] }
+            },
+            {
+            "conversation_id": "2010000000000000001",
+            "in_reply_to_user_id": "300100001",
+            "author_id": "300100005",
+            "created_at": "2025-10-03T18:04:00.000Z",
+            "id": "2010000000000000005",
+            "referenced_tweets": [{ "type": "replied_to", "id": "2010000000000000001" }],
+            "text": "@Fred Easy win",
+            "entities": { "mentions": [{ "start": 0, "end": 5, "username": "Fred", "id": "300100001" }] }
+            },
+            {
+            "conversation_id": "2010000000000000001",
+            "in_reply_to_user_id": "300100001",
+            "author_id": "300100006",
+            "created_at": "2025-10-03T18:05:00.000Z",
+            "id": "2010000000000000006",
+            "referenced_tweets": [{ "type": "replied_to", "id": "2010000000000000001" }],
+            "text": "@Fred What do you think, @BobAi?",
+            "entities": {
+                "mentions": [
+                { "start": 0, "end": 5, "username": "Fred", "id": "300100001" },
+                { "start": 25, "end": 32, "username": "BobAi", "id": "300199999" }
+                ]
+            }
+            }
+        ],
+        "includes": {
+            "users": [
+            { "name": "Fred", "id": "300100001", "username": "Fred" },
+            { "name": "Luke", "id": "300100002", "username": "Luke" },
+            { "name": "Gaz", "id": "300100003", "username": "Gaz" },
+            { "name": "Pete", "id": "300100004", "username": "Pete" },
+            { "name": "Maddy", "id": "300100005", "username": "Maddy" },
+            { "name": "Dan", "id": "300100006", "username": "Dan" },
+            { "name": "Bob AI", "id": "300199999", "username": "BobAi" }
+            ]
+        },
+        "meta": {
+            "newest_id": "2010000000000000006",
+            "oldest_id": "2010000000000000001",
+            "result_count": 6
+        }
+    }
+];
--- controllers/bob/poll-mentions.js
+++ controllers/bob/poll-mentions.js
@@ -1,6 +1,7 @@
 function pollMentions(req, res) {
+    console.log("req.params in pollMentions", req.params);
     console.log("req.body in pollMentions", req.body);
-    res.json({ message: `pollMentions req.body: ${JSON.stringify(req.body)}` });
+    res.json({ message: `pollMentions req.params: ${req.params} || req.body: ${JSON.stringify(req.body)}` });
 }
 
 module.exports = { pollMentions };
